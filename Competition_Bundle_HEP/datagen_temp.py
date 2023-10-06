30e2dfaab546dec5240e8f977ce27bf16d18fd20
Ragansu Chakkappai <ragansu@gmail.com>
2023-10-06 17:28:02 +0200

    bug_fix

--- Competition_Bundle_HEP/datagen_temp.py
+++ Competition_Bundle_HEP/datagen_temp.py
@@ -8,7 +8,7 @@ df = pd.read_csv('./reference_data.csv')
 
 # Remove the "label" and "weights" columns from the data
 label = df.pop('Label')
-weights = df.pop('Weights')
+weights = df.pop('Weight')
 
 # Split the data into training and testing sets
 train_df, test_df, train_label, test_label, train_weights, test_weights = train_test_split(df, label, weights, test_size=0.3)
