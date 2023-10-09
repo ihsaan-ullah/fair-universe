7bdc85c323d0059752dc3cb709893253d4995277
Ragansu Chakkappai <ragansu@gmail.com>
2023-10-09 15:26:05 +0200

    balancing weights
    bug

--- Competition_Bundle_HEP/datagen_temp.py
+++ Competition_Bundle_HEP/datagen_temp.py
@@ -14,8 +14,11 @@ weights = df.pop('Weight')
 print ("sum of weights : ", np.sum(weights))
 print ("sum of signal" , np.sum(weights[label==1]))
 print ("sum of background" , np.sum(weights[label==0]))
-       
-       
+
+# Calculate the sum of weights of signal and background for the whole data
+total_weight = np.sum(weights)
+total_signal_weight = np.sum(weights[label==1])
+total_background_weight = np.sum(weights[label==0])
 
 # Split the data into training and testing sets
 train_df, test_df, train_label, test_label, train_weights, test_weights = train_test_split(df, label, weights, test_size=0.3)
@@ -30,19 +33,55 @@ if not os.path.exists('./input_data/train/weights'):
 if not os.path.exists('./input_data/test/weights'):
     os.makedirs('./input_data/test/weights')
 
+subset_weight = np.sum(train_weights)
+subset_signal_weight = np.sum(train_weights[train_label==1])
+subset_background_weight = np.sum(train_weights[train_label==0])
+
+
+# Adjust the weights of the training set to match the weights of the whole data
+train_weights *= total_weight / subset_weight
+train_weights[train_label==1] *= total_signal_weight / subset_signal_weight
+train_weights[train_label==0] *= total_background_weight / subset_background_weight
+
+print ("sum of signal" , np.sum(train_weights[train_label==1]))
+print ("sum of background" , np.sum(train_weights[train_label==0]))
+
+
+
+
 # Save the label and weight files for the training set
 train_label.to_csv('./input_data/train/labels/data.labels', index=False, header=False)
 train_weights.to_csv('./input_data/train/weights/data.weights', index=False, header=False)
 
-
 # Divide the test set into 10 equal parts and save each part as a separate CSV file
 test_dfs = [test_df[i:i+len(test_df)//10] for i in range(0, len(test_df), len(test_df)//10)]
-for i, test_df in enumerate(test_dfs):
-    test_df.to_csv(f'./input_data/test/data/data_{i}.csv', index=False)
-
 test_weights_ = [test_weights[i:i+len(test_weights)//10] for i in range(0, len(test_weights), len(test_weights)//10)]
-for i, test_weights in enumerate(test_weights_):
+
+for i, (test_df, test_weights) in enumerate(zip(test_dfs, test_weights_)):
+    # Calculate the sum of weights of signal and background for the current subset
+    subset_weight = np.sum(test_weights)
+    subset_signal_weight = np.sum(test_weights[test_label==1])
+    subset_background_weight = np.sum(test_weights[test_label==0])
+    
+    # Adjust the weights of the current subset to match the weights of the whole data
+    test_weights *= total_weight / subset_weight
+    test_weights[test_label==1] *= total_signal_weight / subset_signal_weight
+    test_weights[test_label==0] *= total_background_weight / subset_background_weight
+    
+    # Save the current subset as a CSV file
+    test_df.to_csv(f'./input_data/test/data/data_{i}.csv', index=False)
     test_weights.to_csv(f'./input_data/test/weights/data_{i}.weights', index=False, header=False)
+
+    print ("sum of signal" , np.sum(test_weights[test_label==1]))
+    print ("sum of background" , np.sum(test_weights[test_label==0]))
+
+    
+
+# Save the training set as a CSV file
+train_df.to_csv('./input_data/train/data/data.csv', index=False)
+
+print ("Shape of test set : ",np.shape(test_df))
+print ("Shape of train set : ",np.shape(train_df)) 
     
 
 # Save the training set as a CSV file
