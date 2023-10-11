e3d0c7c46f55abade60b88a7a313fbf27770d37e
Ragansu Chakkappai <ragansu@gmail.com>
2023-10-11 11:25:50 +0200

    test_label error

--- Competition_Bundle_HEP/ingestion_program/ingestion.py
+++ Competition_Bundle_HEP/ingestion_program/ingestion.py
@@ -132,11 +132,13 @@ class Ingestion():
         self.test_sets_weights = []
         for i in range(0, 10):
             test_weights_file = os.path.join(input_dir, 'test', 'weights', 'data_'+str(i)+'.weights')
-            test_labels_file = os.path.join(input_dir, 'test', 'labels', 'data_'+str(i)+'.labels')  
             with open(test_weights_file) as f:
                 self.test_sets_weights.append(np.array(f.read().splitlines(), dtype=float))
+
+        for i in range(0, 10):
+            test_labels_file = os.path.join(input_dir, 'test', 'labels', 'data_'+str(i)+'.labels')
             with open(test_labels_file) as f:
-                self.test_labels = np.array(f.read().splitlines(), dtype=float)
+                self.test_labels.append(np.array(f.read().splitlines(), dtype=float))
 
 
     def initialize_submission(self):
